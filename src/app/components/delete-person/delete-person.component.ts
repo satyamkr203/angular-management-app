0c347028078e6d82eef97b76658cd6dbca06f15b
satyamkr203 <146471886+satyamkr203@users.noreply.github.com>
2025-03-09 11:47:19 +0530

    initial commit

---
+++ src/app/components/delete-person/delete-person.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-person',
+  imports: [],
+  templateUrl: './delete-person.component.html',
+  styleUrl: './delete-person.component.css'
+})
+export class DeletePersonComponent {
+
+}
---
+++ src/app/components/edit-person/edit-person.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-person',
+  imports: [],
+  templateUrl: './edit-person.component.html',
+  styleUrl: './edit-person.component.css'
+})
+export class EditPersonComponent {
+
+}
